6a47f4c9c629cf12f5b95351233caf3cf1d8fca7
Tomasz Paruzel <324010@uwr.edu.pl>
2023-10-07 11:44:57 +0200

    task 2 a,b done

--- List_1/namespaces.hpp
+++ List_1/namespaces.hpp
@@ -1,8 +1,92 @@
-
+#include <iostream>
 
 namespace cpplab {
-    void print(const char* text) 
-    {
+template <typename T> 
+class vector {
+    private:
 
+        // Vector storage capacity
+        int capacity;
+        
+        // Current - number of elements
+        int current;
+            
+        // Integer ponter - stores vector address
+        T* arr;
+
+    public:
+
+        typedef T value_type;
+            
+        // Default constructor
+        vector() {
+            arr = new T[1];
+            capacity = 1;
+            current = 0;
+        }
+
+        // Destructor to dellocate storage
+        // prevents memory leak
+        ~vector() {
+            delete [] arr;
+        }
+
+        // function to add elements
+        void push(T data) {
+
+            if(current == capacity) {
+                T* temp = new T[2 * capacity];
+                //copy 
+                for(int i = 0; i<capacity; i++) {
+                    temp[i] = arr[i];
+                }
+                // deleting previous array 
+                delete[] arr;
+                capacity *= 2;
+                arr = temp;
+            }
+            // inserting data 
+            arr[current] = data;
+            current++;
+        }
+
+        // Function to add data at any index
+        void push(T data, int index) {
+            if( index == capacity) {
+                push(data);
+            }
+            else {
+                arr[index] = data;
+            }
+        }    
+        
+        // function to extract alement at provided index
+        T get(int index) {
+            if(index < current) 
+                return arr[index];
+        }
+
+        // function to delete last element
+        void pop() {
+            current--;
+        }
+
+        //function to get vector size
+        int size() {
+            return current;
+        }
+
+        //function to get vector capacity
+        int getcapacity() {
+            return capacity;
+        } 
+
+        //function to get all vector elements
+        void print() {
+            for (int i=0; i<current; i++) {
+                std::cout << arr[i] << " ";
+            }
+            std::cout << std::endl;
+        }
     };
 }
--- List_1/task_2.cpp
+++ List_1/task_2.cpp
@@ -1,5 +1,18 @@
 #include "namespaces.hpp"
 
-main() {
+int main() {
+    cpplab::vector<int> V;
 
+    V.push(1);
+    V.push(3);
+    V.push(4);
+    V.push(6);
+
+    V.print();
+
+    V.pop();
+    V.print();
+    
+    std::cout<< "Capacity: "<< V.getcapacity() <<std::endl;
+    std::cout<< "Size: "<<V.size() << std::endl;
 }

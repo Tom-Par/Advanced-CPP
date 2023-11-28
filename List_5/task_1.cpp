c136b9d4e0d006321c2ac8884acb44e4fe55f365
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-11-28 17:28:38 +0100

    mutex - attempt

--- List_5/task_1.cpp
+++ List_5/task_1.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
+#include <mutex>    
+#include <future>
 
-void foo() {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+int thread_id(){
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> lock(mutex);
+    static int i = 0; 
+    thread_local int id = ++i;
+    return i;
 }
 
+void print_text(std::string text) {
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> lock(mutex);
+    std::cout << "Thread " << thread_id() << ": "<< text<<std::endl;
+}
+
+
 int main() {
-    std::thread t1(foo);
-    std::thread::id t1_id = t1.get_id();
+    std::thread th1( &print_text, "Jeden");
+    std::thread th2( &print_text, "Dwa");
+    std::thread th3( &print_text, "Trzy");
+    std::thread th4( &print_text, "Cztery");
+    std::thread th5( &print_text, "Pięć");
+    std::thread th6( &print_text, "Sześć");
+    std::thread th7( &print_text, "Siedem");
 
-    std::cout << "t1's id: " << t1_id<<'\n';
-
-    t1.join();
-
-    std::cout << "t1's id after join: " << t1.get_id()<<'\n';
+    th1.join();
+    th2.join();
+    th3.join();
+    th4.join();
+    th5.join();
+    th6.join();
+    th7.join();
 }

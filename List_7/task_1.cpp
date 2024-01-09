3b78b58ba8d8a863cdbbff69ad6234db300884b4
Tomasz Paruzel <tom.krzy.par@gmail.com>
2024-01-09 17:23:10 +0100

    fueltank class done

--- List_7/task_1.cpp
+++ List_7/task_1.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <memory>
+#include <utility>
+
+class FuelTank {
+    private:
+    
+    unsigned int fuel;
+    std::mutex mutex;
+
+    public:
+
+    FuelTank(unsigned int initialFuel) : fuel(initialFuel) {}
+
+    unsigned int refuel(unsigned int amount) 
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        if (fuel >= amount) {
+            fuel -= amount;
+            return amount;
+        } else {
+            return 0;
+        }
+    }
+};
+
+int main() {
+    
+}
